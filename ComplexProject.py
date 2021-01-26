d4719bd44b28cb44b6b4b4f39e0897c5f164e707
Adam Brzosko <47273267+ab7018@users.noreply.github.com>
2021-01-26 22:03:34 +0000

    added task2 (some stats work)

--- ComplexProject.py
+++ ComplexProject.py
@@ -7,23 +7,37 @@ Created on Tue Jan 19 17:11:28 2021
 """
 
 import numpy as np
-L = 16
-P = 0.5
-SteadyState = 1000
-RunTime = 10000
+import matplotlib.pyplot as plt
+import sys
 
-slopes = np.zeros(L)
-thresholds = np.zeros(L)
-changes = []
-h1 = []
-for i in range(len(thresholds)):
+L = 16                  ##################
+P = 0.5                 #input parameters#
+RunTime = 60000         ##################
+
+SteadyState = False             #################
+slopes = np.zeros(L)            #initialisations#
+thresholds = np.zeros(L)        #################
+
+changes = []    #keeps track of which sites relaxed
+h1 = []         #height of site 1 (task 1)
+totH = 0        #total sum of heights
+totT = 0        #total time since the beginning
+pileH = []      #list for total heights at times totT
+
+for i in range(len(thresholds)):             #randomise thresholds
     thresholds[i] = np.random.choice([1,2], p=[P,1-P])
     
 
 def Drive(GrainNo = 1):
+    global totH
+    global totT
     slopes[0] = slopes[0] + GrainNo
+    totH += GrainNo
+    totT += 1
 
-def RelaxAll():
+def RelaxAll(L=4):
+    global totH
+    global SteadyState
     if slopes[0]>thresholds[0]:
         slopes[0] -= 2
         slopes[1] += 1
@@ -45,8 +59,12 @@ def RelaxAll():
         thresholds[L-1] = np.random.choice([1,2], p=[P,1-P])
         changes.append(L-1)
         changes.append(L-2)
+        SteadyState = True
+        totH -= 1
 
-def RelaxSelect(a=[]):
+def RelaxSelect(L=4, a=[]):
+    global totH
+    global SteadyState
     newChanges = []
     for i in range(0,len(a)):
         if a[i] == 0:
@@ -72,25 +90,33 @@ def RelaxSelect(a=[]):
                 thresholds[L-1] = np.random.choice([1,2], p=[P,1-P])
                 newChanges.append(L-1)
                 newChanges.append(L-2)
+                SteadyState = True
+                totH -= 1
     return newChanges
     
-def Run(relPeriods = 1000):
+def Run(L, relPeriods = 1000):
+    '''Executes the simulation with standard #drives = 1000'''
+    global totH
     global changes
     for i in range(relPeriods):
         Drive()
-        RelaxAll()
+        RelaxAll(L)
         changes = list(dict.fromkeys(changes)) #gets rid of repeats
-        #print('C',changes)
+        #print('C',changes)  #debug
         while changes != []:
-            changes = list(dict.fromkeys(RelaxSelect(changes))) #gets rid of repeats
-            #print('CC',changes)
-            #print('SS',slopes)
-            #print('T',thresholds)
-        if RunTime > SteadyState:
+            changes = list(dict.fromkeys(RelaxSelect(L, changes))) #gets rid of repeats
+            #print('CC',changes)            ########
+            #print('SS',slopes)             #debugs#
+            #print('T',thresholds)          ########
+        if SteadyState == True:
             h1.append(sum(slopes))
-        #print(h1)
-        #print('S',slopes)
-
+            break
+        #print(h1)          #debug
+        #print('S',slopes)  #debug
+        pileH.append(totH)
+        
+'''
+Task 1
 u = []
 var = []
 for i in range(0,10):
@@ -106,5 +132,61 @@ for i in range(0,10):
         thresholds[i] = np.random.choice([1,2], p=[P,1-P])
 
 print('The average height is:', sum(u)/len(u), '+-', np.sqrt(sum(var)/len(var)))
+'''
+'''
+Task 2a
+
+datax = {}
+datay = {}
+for i in [4, 8, 16, 32, 64, 128, 256]:
+    pileH = []
+    totT = 0
+    totH = 0
+    changes = []
+    h1 = []
+    SteadyState = False             #################
+    slopes = np.zeros(i)            #initialisations#
+    thresholds = np.zeros(i)        #################
+    for k in range(len(thresholds)):             #randomise thresholds
+        thresholds[k] = np.random.choice([1,2], p=[P,1-P])
+
+    Run(i, RunTime)
+    datax['x{0}'.format(i)] = list(range(totT))
+    datay['y{0}'.format(i)] = pileH
+
+plt.plot(datax['x4'],datay['y4'], label = 'L = 4')
+
+plt.plot(datax['x8'],datay['y8'], label = 'L = 8')
+plt.plot(datax['x16'],datay['y16'], label = 'L = 16')
+plt.plot(datax['x32'],datay['y32'], label = 'L = 32')
+plt.plot(datax['x64'],datay['y64'], label = 'L = 64')
+plt.plot(datax['x128'],datay['y128'], label = 'L = 128')
+plt.plot(datax['x256'],datay['y256'], label = 'L = 256')
+plt.title('Time to reach steady state')
+plt.xlabel('Time since beginning of simulation')
+plt.ylabel('Height of pile')
+plt.legend(loc = 'upper left')
+'''
+
+'''
+Task2b
+'''
+times = []
+crossover = []
+for i in [4, 8, 16, 32, 64, 128, 256]:
+    pileH = []
+    totT = 0
+    totH = 0
+    changes = []
+    h1 = []
+    SteadyState = False             #################
+    slopes = np.zeros(i)            #initialisations#
+    thresholds = np.zeros(i)        #################
+    for k in range(len(thresholds)):             #randomise thresholds
+        thresholds[k] = np.random.choice([1,2], p=[P,1-P])
+
+    Run(i, RunTime)
+    times.append(totT)
+    crossover.append(totT/i)
 
 
