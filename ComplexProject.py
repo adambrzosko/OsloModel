5f69c7b5fb1ed1a9c85b2f154562de8c5ad49ac9
Adam Brzosko <47273267+ab7018@users.noreply.github.com>
2021-02-04 15:03:23 +0000

    up to 2g

--- ComplexProject.py
+++ ComplexProject.py
@@ -8,10 +8,12 @@ Created on Tue Jan 19 17:11:28 2021
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
+from scipy.optimize import curve_fit as cf
 
 L = 16                  ##################
 P = 0.5                 #input parameters#
-RunTime = 70000         ##################
+RunTime = 100000         ##################
 
 SteadyState = False             #################
 slopes = np.zeros(L)            #initialisations#
@@ -87,10 +89,11 @@ def Run(L, relPeriods = 1000):
         changes = list(dict.fromkeys(changes)) #gets rid of repeats
         while changes != []:
             changes = list(dict.fromkeys(RelaxSelect(L, changes))) #gets rid of repeats
-        h1.append(sum(slopes))
+        #h1.append(sum(slopes))
         if SteadyState == True:
-         #   break
-         tc.append(totT)
+            #   break
+            tc.append(totT)
+            h1.append(sum(slopes))
         pileH.append(totH)
 #%%        
 '''
@@ -177,7 +180,6 @@ datax = {}
 datay = {}
 
 for i in [4, 8, 16, 32, 64, 128, 256]:
-    tcs = []           #list of crossover times for scaling
     heights = []       #list of heights on iterations
     for k in range(10):
         totT = 0
@@ -191,15 +193,12 @@ for i in [4, 8, 16, 32, 64, 128, 256]:
             thresholds[k] = np.random.choice([1,2], p=[P,1-P])
 
         Run(i, RunTime)             
-        tcs.append(tc[0])
         heights.append(h1)
-    t_c = np.mean(tcs)
-    scaled_t = [x/t_c for x in list(range(RunTime))]
+    scaled_t = [x/(i*i) for x in list(range(RunTime))]
     scaled_h = [x/i for x in np.mean(heights, axis = 0)]
     datax['x{0}'.format(i)] = scaled_t
     datay['y{0}'.format(i)] = scaled_h
         
-    '''like averaging graphs then scaling them, will have a think how (h/L vs t/tc)'''
 datax['x4'].pop(0)
 datay['y4'].pop(0)
 datax['x8'].pop(0)
@@ -214,15 +213,97 @@ datax['x128'].pop(0)
 datay['y128'].pop(0)
 datay['y256'].pop(0)
 datax['x256'].pop(0)
+
+with open('/home/adam/Desktop/work/3rd Year/Complexity&Networks/Oslo model project/2dDatax', 'wb') as f:
+    pickle.dump(datax,f)
+with open('/home/adam/Desktop/work/3rd Year/Complexity&Networks/Oslo model project/2dDatay', 'wb') as f:
+    pickle.dump(datay,f)
     
-plt.plot(np.log(datax['x4']),np.log(datay['y4']), label = 'L = 4')
-plt.plot(np.log(datax['x8']),np.log(datay['y8']), label = 'L = 8')
-plt.plot(np.log(datax['x16']),np.log(datay['y16']), label = 'L = 16')
-plt.plot(np.log(datax['x32']),np.log(datay['y32']), label = 'L = 32')
-plt.plot(np.log(datax['x64']),np.log(datay['y64']), label = 'L = 64')
-plt.plot(np.log(datax['x128']),np.log(datay['y128']), label = 'L = 128')
-plt.plot(np.log(datax['x256']),np.log(datay['y256']), label = 'L = 256')
+plt.plot(datax['x4'],datay['y4'], label = 'L = 4')
+plt.plot(datax['x8'],datay['y8'], label = 'L = 8')
+plt.plot(datax['x16'],datay['y16'], label = 'L = 16')
+plt.plot(datax['x32'],datay['y32'], label = 'L = 32')
+plt.plot(datax['x64'],datay['y64'], label = 'L = 64')
+plt.plot(datax['x128'],datay['y128'], label = 'L = 128')
+plt.plot(datax['x256'],datay['y256'], label = 'L = 256')
 plt.title('Data collapse for the scaled height')
-plt.xlabel('Time/cross-over time')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Time/(system size)^2')
 plt.ylabel('Height/system size')
 plt.legend(loc = 'upper left')
+
+#%%
+'''
+tasks 2e-2g data
+'''
+heights = []
+stdevs = []
+heightProbs = {}
+for i in [4, 8, 16, 32, 64, 128, 256]:
+    totT = 0
+    changes = []
+    h1 = []
+    tc = []
+    SteadyState = False             #################
+    slopes = np.zeros(i)            #initialisations#
+    thresholds = np.zeros(i)        #################
+    for k in range(len(thresholds)):             #randomise thresholds
+        thresholds[k] = np.random.choice([1,2], p=[P,1-P])
+
+    Run(i, RunTime)             
+    heights.append(sum(h1)/(tc[-1]-tc[0]))
+    stdevs.append(np.sqrt(sum(map(lambda x: x*x, h1))/(tc[-1]-tc[0])-
+                  (sum(h1)/(tc[-1]-tc[0]))**2))
+    for j in range(int(min(h1)), int(max(h1))+1):
+        heightProbs['L{}-H{}'.format(i,j)]= h1.count(j)  
+        
+with open('/home/adam/Desktop/work/3rd Year/Complexity&Networks/Oslo model project/2eHeights', 'wb') as f:
+    pickle.dump(heights,f)
+with open('/home/adam/Desktop/work/3rd Year/Complexity&Networks/Oslo model project/2eStdevs', 'wb') as f:
+    pickle.dump(stdevs,f)
+with open('/home/adam/Desktop/work/3rd Year/Complexity&Networks/Oslo model project/2eHeightProbs', 'wb') as f:
+    pickle.dump(heightProbs,f)
+    
+#%%
+'''
+task 2e
+'''
+with open('/home/adam/Desktop/work/3rd Year/Complexity&Networks/Oslo model project/2eHeights', 'rb') as f:
+   heights = pickle.load(f)
+
+def Corr(L, a0, a1, w1):
+    H = a0*L - a0*a1*(L**(1-w1))
+    return H
+
+S = np.array([4, 8, 16, 32, 64, 128, 256])
+popt, pcov = cf(Corr, S, heights)
+
+print(popt, pcov)
+plt.plot(S, heights, '--bo', label = 'data')
+plt.plot(S, Corr(S,*popt), '-r', label = 'fit')
+plt.xlabel('System size')
+plt.ylabel('Average height')
+plt.title('Time-averaged heights against system size in steady state')
+plt.legend()
+plt.show()
+
+#%%
+'''
+task 2f
+'''
+with open('/home/adam/Desktop/work/3rd Year/Complexity&Networks/Oslo model project/2eStdevs', 'rb') as f:
+   stdevs = pickle.load(f)
+S = np.array([4, 8, 16, 32, 64, 128, 256])
+
+plt.plot(S, stdevs, '--bo', label = 'data')
+plt.xlabel('System size')
+plt.ylabel('Average height')
+plt.title('Standard deviation of average heights against system size')
+plt.legend()
+plt.show()
+   
+#%%
+'''
+task 2g
+'''
